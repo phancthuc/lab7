df45261576da8eb5c6924966aa99809752a4daeb
Thuc Phan <tcp004@ucsd.edu>
2020-02-19 22:04:33 -0800

    analytics functions

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,5 +9,10 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
+	$(".likeBtn").click(likeBtnListener);
 	// your code here
+}
+
+function likeBtnListener(){
+	ga("send", "event", "like", "click");
 }
